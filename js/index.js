1d33dc40f501c068dadf6f613bdc5b3eb70c7962
Eduardo <eduardo.fuso@novaandradina.org>
2019-04-04 13:23:03 -0400

    Comentando código

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var values = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-        var chave = +document.getElementById("chave").innerHTML;
+var values = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"); //array do alfabeto
+        var chave = +document.getElementById("chave").innerHTML; //captura a chave do input com id chave
 
        
 
@@ -11,15 +11,15 @@ var values = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
                 var result = "";
                 var chave = parseInt(document.getElementById('chave').value)
 
-                for(var i = 0; i<palavra.length; i++){ //Passa por cada caracter do input
+                for(var i = 0; i<palavra.length; i++){ //for para ler todos caracteres da palavra digitada de acordo com o tamanho da palavra
 
-                    var posicaoDaLetraNoAlfabeto = palavra.charCodeAt(i)-64; //Identifica qual letra é do alfabeto
-                    var letraComDeslocamento = (posicaoDaLetraNoAlfabeto + chave) % 26; //Faz o deslocamento de César e mantém dentro do alfabeto (26 letras)
-                    letraComDeslocamento = letraComDeslocamento == 0 ? 26 : letraComDeslocamento; //MOD retornar 0 caso o resultado seja 26, tem que tratar isso
-                    result += values[letraComDeslocamento-1]; //Faz -1 porque a letra 1 (A) está no índice 0 do teu array.
+                    var posicaoDaLetraNoAlfabeto = palavra.charCodeAt(i)-64; //usa formula baseada na tabela asc para descobir o caractere digitado
+                    var letraComDeslocamento = (posicaoDaLetraNoAlfabeto + chave) % 26; //aplica o deslocamento da cifra de césar
+                    letraComDeslocamento = letraComDeslocamento == 0 ? 26 : letraComDeslocamento; 
+                    result += values[letraComDeslocamento-1]; //-1 porque a primeira letra(A) esta na posição 0 do vetor
                 }
 
-                document.getElementById("saidaCrip").innerHTML = result;
+                document.getElementById("saidaCrip").innerHTML = result; //saida da palavra criptografada
             }
 
             function descriptografar() {
@@ -27,12 +27,12 @@ var values = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
                 var result = "";
                 var chave = parseInt(document.getElementById('chave').value)
 
-                for(var i = 0; i<palavra.length; i++){ //Passa por cada caracter do input
+                for(var i = 0; i<palavra.length; i++){ 
 
-                    var posicaoDaLetraNoAlfabeto = palavra.charCodeAt(i)-64; //Identifica qual letra é do alfabeto
-                    var letraComDeslocamento = (posicaoDaLetraNoAlfabeto - chave) % 26; //Faz o deslocamento de César e mantém dentro do alfabeto (26 letras)
-                    letraComDeslocamento = letraComDeslocamento == 0 ? 26 : letraComDeslocamento; //MOD retornar 0 caso o resultado seja 26, tem que tratar isso
-                    result += values[letraComDeslocamento-1]; //Faz -1 porque a letra 1 (A) está no índice 0 do teu array.
+                    var posicaoDaLetraNoAlfabeto = palavra.charCodeAt(i)-64;
+                    var letraComDeslocamento = (posicaoDaLetraNoAlfabeto - chave) % 26;
+                    letraComDeslocamento = letraComDeslocamento == 0 ? 26 : letraComDeslocamento; 
+                    result += values[letraComDeslocamento-1]; 
                 }
 
                 document.getElementById("saidaDescrip").innerHTML = result;
